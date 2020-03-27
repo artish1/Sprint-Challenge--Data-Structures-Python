159914d720e68fe88fafa8bdc2c255b48ab61f68
Mark Artishuk <markyshuk@gmail.com>
2020-03-27 08:35:39 -0700

    Finished ring buffer, commented stretch tests, will uncomment if use later

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,15 +8,35 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if self.current is None:
+            self.storage.add_to_head(item)
+            self.current = self.storage.head
+            return
+
+        if len(self.storage) == self.capacity:
+            if self.current.next is None:
+                self.storage.remove_from_head()
+                self.storage.add_to_head(item)
+                self.current = self.storage.head
+            else:
+                self.current = self.current.next
+                self.current.value = item
+        else:
+            self.storage.add_to_tail(item)
+            self.current = self.storage.tail
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
-
+        current_node = self.storage.head
+        while current_node:
+            if not current_node is None and not current_node.value is None:
+                list_buffer_contents.append(current_node.value)
+            current_node = current_node.next
         return list_buffer_contents
+
 
 # ----------------Stretch Goal-------------------
 
